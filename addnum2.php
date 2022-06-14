e490b16fdf7a18cd1f7b3cfc8d824667a734cbec
Ranjeeta Gogoi <ranjeetagogoi20@gmail.com>
2022-06-14 10:24:53 +0530

    Add files via upload
    Addition of two numbers using pass by value.

---
+++ addnum2.php
@@ -0,0 +1,9 @@
+<?php
+        function addnum($a, $b){
+                $sum = $a + $b;
+                return $sum;
+        }
+        
+        $add= addnum(70, 80);
+        echo "70+80 = $add<br>";
+?>
