599c3d3cf74eb50a4464801da11befbd7921c856
Ranjeeta Gogoi <ranjeetagogoi20@gmail.com>
2022-06-14 10:17:54 +0530

    Add files via upload
    Addition of two numbers by assigning the values to the variables within the function itself.

---
+++ addnum1.php
@@ -0,0 +1,10 @@
+<?php
+        function addnum(){
+                $a = 5;
+                $b = 6;
+                $sum = $a + $b;
+                echo"SUM = $sum <br>";
+        }
+        
+        addnum();
+?>
